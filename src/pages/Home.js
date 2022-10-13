f5bc98860bb64307d8dd2b691ef9c021e1d630d0
HarshathMohanaSundaram <harshathsundaram@gmail.com>
2022-10-13 11:37:51 +0530

    update button and functions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,13 @@ import React, {useEffect,useState} from 'react'
 import './Home.css'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Home = () => {
 
@@ -30,40 +37,42 @@ const Home = () => {
       <Link to="/Addcontact">
       <button className='btn btn-contact'>Add Contact</button>
       </Link>
-       <table className='styled-table'>
-          <thead>
-            <tr>
-              <th style={{textAlign:'center'}}>No.</th>
-              <th style={{textAlign:'center'}}>Name</th>
-              <th style={{textAlign:'center'}}>Email</th>
-              <th style={{textAlign:'center'}}>Contact</th>
-              <th style={{textAlign:'center'}}>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item,index)=>{
-              return(
-                <tr key={item.id}>
-                <th scope='row'>{index+1}</th>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.contact}</td>
-                <td>
-                  <Link to={`/update/${item.id}`}>
+      <TableContainer component={Paper} className="styled-table">
+        <Table  aria-label="caption table">
+          <caption>Contact data in database</caption>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">No.</TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">Contact</TableCell>
+              <TableCell align="center">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((item,index) => (
+              <TableRow key={item.id}>
+                <TableCell component="th" scope="row">{index+1}</TableCell>
+                <TableCell align="right">{item.name}</TableCell>
+                <TableCell align="right">{item.email}</TableCell>
+                <TableCell align="right">{item.contact}</TableCell>
+                <TableCell align="right">
+                <Link to={`/update/${item.id}`}>
                     <button className='btn btn-edit'>Edit</button>
                   </Link>
                   <button className='btn btn-delete' onClick={()=>deleteContact(item.id)}>Delete</button>
                   <Link to={`/view/${item.id}`}>
                     <button className='btn btn-view'>View</button>
                   </Link>
-                </td>
-              </tr>
-              );
-            })}
-          </tbody>
-       </table>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
 
 export default Home
+
